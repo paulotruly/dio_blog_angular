657cfe92e8f3faa8a4bb41e32baf3fd1556479c3
Paulo <paulomsmsm@gmail.com>
2025-03-15 01:47:03 -0300

    aula11

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { BigCardComponent } from "../../components/big-card/big-card.component";
+import { SmallCardComponent } from "../../components/small-card/small-card.component";
+import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [BigCardComponent, SmallCardComponent, MenuTitleComponent, MenuBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  @Input()
+  photoCover:string=""
+  @Input()
+  cardTitle:string=""
+  @Input()
+  cardDescription:string=""
 
 }
